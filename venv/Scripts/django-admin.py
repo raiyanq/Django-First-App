66c13d8e5d9c3209acc592b26ab1296e4c1a4a27
Raiyan <raiyan.quaium@carleton.ca>
2018-08-03 21:47:14 -0400

    First commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Raiyan\Documents\GitHub\Django-First-App\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
