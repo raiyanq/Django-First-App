b66e942f96c865da7c31de190d722e1c606cc925
Raiyan <raiyan.quaium@carleton.ca>
2018-08-04 14:57:28 -0400

    created index page and models

--- mywebsite/polls/models.py
+++ mywebsite/polls/models.py
@@ -6,7 +6,13 @@ class Question(models.Model):
     question_text = models.CharField(max_length=100)
     pub_date = models.DateTimeField('date_published')
 
+    def _str_(self):
+        return self.question_text
+
 class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def _str_(self):
+        return self.choice_text
