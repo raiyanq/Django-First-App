f42085079338e2b7e0fffd1b0bb8dfdb7c353557
Raiyan <raiyan.quaium@carleton.ca>
2018-08-05 17:40:50 -0400

    Created Html for the questions and Html Template with header and footer

--- mywebsite/polls/views.py
+++ mywebsite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse
 
@@ -14,8 +14,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("This is the detailed view of the question: %s" % question_id)
-
+    question =get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     return HttpResponse("These are the results of the question: %s" % question_id)
