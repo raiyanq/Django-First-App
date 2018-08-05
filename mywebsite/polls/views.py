0d3980471705a4dbb6993dfa8eb909d431b9a5c9
Raiyan <raiyan.quaium@carleton.ca>
2018-08-05 01:23:05 -0400

    Added index html page

--- mywebsite/polls/views.py
+++ mywebsite/polls/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+
+from .models import Question
+
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Awesome Guys, THis is the index page')
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_questions': latest_questions}
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    return HttpResponse("This is the detailed view of the question: %s" % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("These are the results of the question: %s" % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("Vote on question: %s" % question_id)
